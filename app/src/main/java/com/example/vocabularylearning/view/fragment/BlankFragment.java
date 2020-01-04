477f8374e76cf72d3444b87e4adc9070452d4dc3
truongloc <nmtruongloc806@gmail.com>
2020-01-04 14:58:23 +0700

    commit first

---
+++ app/src/main/java/com/example/vocabularylearning/view/fragment/BlankFragment.java
@@ -0,0 +1,34 @@
+package com.example.vocabularylearning.view.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vocabularylearning.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        return view;
+    }
+
+}
